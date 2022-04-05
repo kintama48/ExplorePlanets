7d878b3ec4d3d652582cb429f42b6024f75ef9fd
kintama <rebelschaos@gmail.com>
2022-04-06 01:48:58 +0500

    latest

--- app/src/main/java/com/example/planets/MainActivity.kt
+++ app/src/main/java/com/example/planets/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.planets.databinding.ActivityMainBinding
 
 
-lateinit var binding: ActivityMainBinding
+//lateinit var binding: ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
